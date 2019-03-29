f41bf4b9c9ed9215eeb5abd3a823b1cdf76053fe
Dan Leehr <dan.leehr@duke.edu>
2019-03-29 13:52:15 -0400

    Remove files not related to exomeseq-gatk3

--- examples/tools/run_add_read_groups.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-mkdir -p data
-cwltool --outdir ./data tools/picard-AddOrReplaceReadGroups.cwl samples/add-read-groups.json
-
--- examples/tools/run_sortsam.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p data
-cwltool --outdir ./data tools/picard-SortSam.cwl samples/sortsam.json

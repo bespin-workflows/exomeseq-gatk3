e02881aa33c73a93cd134dc1bdd2df323e00b5be
Dan Leehr <dan.leehr@duke.edu>
2019-03-29 13:52:15 -0400

    Remove files not related to exomeseq-gatk3

--- examples/tools/run_qc-gz.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p data
-cwltool --outdir ./data tools/fastqc.cwl samples/qc-gz.json
--- examples/tools/run_quant.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p data
-cwltool --outdir ./data tools/htseq-count.cwl samples/quant.json
